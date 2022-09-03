04530d07876df1503943ddd02bbf5675dc1d1cad
Somin <sominupadhyaya@gmail.com>
2022-09-03 09:04:01 +0545

    complete basic structure of app

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React , {useReducer} from 'react'
+
+type ActionTypes = {
+  ADD_NUMS: string,
+  OPERATION: string,
+  CLEAR_SCREEN : string,
+  DELETE_NUMS : string,
+  EVALUATE : string
+}
+interface State {
+  currentOperand : number,
+  previousOperand : number,
+  operation : string
+}
+
+const reducer = (state : State, action) =>{
+
+}
 
 const Calculator = () => {
+  const [{currentOperand, previousOperand, operation} , dispatch] = useReducer<State>(reducer , {})
   return (
     <div>
+      <div className="previous-operand">{previousOperand} {operation}</div>
+      <div className="current-operand">{currentOperand}</div>
       <button className="normal-btns" >1</button>
       <button className="normal-btns" >2</button>
       <button className="normal-btns" >3</button>
