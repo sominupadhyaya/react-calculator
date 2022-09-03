743705b0f00959de7f71d33b8d45e5d1c03fb638
Somin <sominupadhyaya@gmail.com>
2022-09-03 08:43:22 +0545

    scrap the previous idea and start with another one

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,29 +1,22 @@
-import React , {useState} from 'react'
+import React from 'react'
 
 const Calculator = () => {
-  const [userEnteringNums, setUserEnteringNums] = useState(true)
-  const [numberOne, setNumberOne] = useState<number | null>(null)
-  const [numberTwo, setNumberTwo] = useState<number | null>(null)
-
-  const handleClick = (num1 : number) => () => setNumberOne(num1)
-
-  const handleOperations = (e) => () =>{
-    setUserEnteringNums(false)
-  }
-
   return (
     <div>
-      <button className="normal-btns" onClick={handleClick(1)}>1</button>
-      <button className="normal-btns" onClick={handleClick(2)}>2</button>
-      <button className="normal-btns" onClick={handleClick(3)}>3</button>
-      <button className="normal-btns" onClick={handleClick(4)}>4</button>
-      <button className="normal-btns" onClick={handleClick(5)}>5</button>
-      <button className="normal-btns" onClick={handleClick(6)}>6</button>
-      <button className="normal-btns" onClick={handleClick(7)}>7</button>
-      <button className="normal-btns" onClick={handleClick(8)}>8</button>
-      <button className="normal-btns" onClick={handleClick(9)}>9</button>
-      <button className="normal-btns" onClick={handleClick(0)}>0</button>
-      <button className="add-btn"     onClick={(e : Event) => console.log(e``)}>+</button>
+      <button className="normal-btns" >1</button>
+      <button className="normal-btns" >2</button>
+      <button className="normal-btns" >3</button>
+      <button className="normal-btns" >4</button>
+      <button className="normal-btns" >5</button>
+      <button className="normal-btns" >6</button>
+      <button className="normal-btns" >7</button>
+      <button className="normal-btns" >8</button>
+      <button className="normal-btns" >9</button>
+      <button className="normal-btns" >0</button>
+      <button className="add-btn">+</button>
+      <button className="sub-btn">-</button>
+      <button className="divide-btn">/</button>
+      <button className="multiply-btn">*</button>
     </div>
   )
 }
